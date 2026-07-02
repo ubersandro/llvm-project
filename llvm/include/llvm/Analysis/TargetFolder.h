3c7bf4a2ac58fa693deb27bdee0f932727ce038b
ubersandro <alessandromileto22@gmail.com>
2026-07-02 12:24:04 +0000

    TargetFolder must stay there AS IS for now

--- llvm/include/llvm/Analysis/TargetFolder.h
+++ llvm/include/llvm/Analysis/TargetFolder.h
@@ -117,7 +117,7 @@ public:
 
   Value *FoldGEP(Type *Ty, Value *Ptr, ArrayRef<Value *> IdxList,
                  GEPNoWrapFlags NW) const override {
-    return nullptr;
+    return nullptr; // FSAN
     if (!ConstantExpr::isSupportedGetElementPtr(Ty))
       return nullptr;
 
